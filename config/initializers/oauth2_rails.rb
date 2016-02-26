af2c2f32422fa995d09c0e7f4bab6cf7cc585de7
ColMac <cmaccannell@gmail.com>
2016-02-26 13:42:10 -0800

    update oauth

--- config/initializers/oauth2_rails.rb
+++ config/initializers/oauth2_rails.rb
@@ -1,3 +1,3 @@
-OAUTH2_RAILS_ID       = ENV['OAUTH2_RAILS_ID']
-OAUTH2_RAILS_SECRET   = ENV['OAUTH2_RAILS_SECRET']
-OAUTH2_RAILS_CALLBACK = 'http://fitbit-colin.herokuapp.com/oauth2_callbacks/fitbit'
+OAUTH2_RAILS_ID       = '227H3Q'
+OAUTH2_RAILS_SECRET   = '3007a94e8523ca5def62d3654e3b4be1'
+OAUTH2_RAILS_CALLBACK = 'https://fierce-mountain-32343.herokuapp.com/oauth2_callbacks/fitbit'
